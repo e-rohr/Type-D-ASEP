57ade2f10da8a5200d74166c037f90870d16cb4d
slkar <slkarthik@hotmail.com>
2023-06-26 09:17:55 -0500

    commit

--- central_element_functions.py
+++ central_element_functions.py
@@ -1,4 +1,5 @@
 from sympy import *
+from numpy import *
 import itertools
 from collections import deque
 f = open('sosoDualHypNumerical.txt', 'w')
@@ -24,7 +25,7 @@ def inverse(M):
     coM = zeros(size)
     signRow = 1
     for i in range(size):
-        signCol = signRow       #
+        signCol = signRow       
         for j in range(size):
             minor = M[:,:]
             minor.col_del(j)
@@ -35,107 +36,7 @@ def inverse(M):
     adjM = coM.T
     return (1/detM)*adjM      
 
-def a(i, j): # Cartan matrix lookup
-    if (i == j):
-        return 2
-    if (abs(i-j) == 1 and max(i, j) <= n-1):
-        return -1
-    if (i == n-2 and j == n or i == n and j == n-2):
-        return -1
-    return 0
 
-def pair(list1, list2):
-    ret = []
-    if(len(list1) != len(list2)):
-        return []
-    if(len(list1) == 1):
-        if(list1[0] == list2[0]):
-            return [0]
-        else:
-            return []
-    first = list2[0]
-    for i in range(len(list1)):
-        if(list1[i] == first):
-            inductive_list1 = list1.copy()
-            inductive_list1.pop(i)
-            inductive_list2 = list2.copy()
-            inductive_list2.pop(0)
-            ih = pair(inductive_list1, inductive_list2)
-            prefactors = 0
-            for j in range(i):
-                prefactors += a(list1[j], list1[i])
-            for j in range(len(ih)):
-                ih[j] += prefactors
-            ret.extend(ih)
-    return ret
-
-def mat(listofLists):
-    M = zeros(len(listofLists))
-    for i in range(len(listofLists)):
-        for j in range(len(listofLists)):
-            lst = pair(listofLists[i], listofLists[j])
-            for k in lst:
-                M[i, j] += q**k
-    return M.applyfunc(simplify)
-
-def dual(listofLists): # for computing specific dual elements
-    M = zeros(len(listofLists))
-    for i in range(len(listofLists)):
-        for j in range(len(listofLists)):
-            lst = pair(listofLists[i], listofLists[j])
-            for k in lst:
-                M[i, j] += q**k
-    N= inverse(M)
-    return N.applyfunc(simplify)
-
-def perm(tentlist): # given list of lists, removes linear dependence
-    fin = []
-    for i in range(len(tentlist)):
-        fin1 = list(fin)
-        fin1.append(tentlist[i])
-        M = mat(fin1)
-        if(M.det() != 0):
-            fin.append(tentlist[i])
-    return fin
-
-def result(setofindices): # gives a basis of elements (the first element is the basis is setofindices)
-    tentlist = list(dict.fromkeys(itertools.permutations(setofindices)))
-    for i in range(len(tentlist)):
-        tentlist[i] = list(tentlist[i])
-    tentlist = reduce(tentlist)
-    return perm(tentlist)
-
-def reduce(tentlist): # remove duplicates
-    visited=[0 for i in range(len(tentlist))]
-    adj = [[0 for i in range(len(tentlist))] for j in range(len(tentlist))]
-    finlist = []
-    for n in range(len(tentlist)):
-        l = tentlist[n]
-        for ind in range(len(l) - 1):
-            if(a(l[ind], l[ind+1]) == 0):
-                ledit = l[:]
-                ledit[ind], ledit[ind+1] = ledit[ind+1], ledit[ind]
-                m = tentlist.index(ledit)
-                adj[m][n] = 1
-                adj[n][m] = 1
-    adjacents = [[] for i in range(len(tentlist))]
-    for i in range(len(tentlist)):
-        temp = []
-        for j in range(len(tentlist)):
-            if(adj[i][j] == 1):
-                temp.append(j)
-        adjacents[i] = temp
-    for n in range(len(tentlist)):
-        if(visited[n] == 0):
-            finlist.append(tentlist[n])
-            qu = deque([n])
-            while(qu):
-                curr = qu.popleft()
-                for neigh in adjacents[curr]:
-                    if(visited[neigh] == 0):
-                        visited[neigh] = 1
-                        qu.append(neigh)
-    return finlist 
 
 # [1, 2, 3] -> E1 E2 E3
 def indicesToE(setofindices):
